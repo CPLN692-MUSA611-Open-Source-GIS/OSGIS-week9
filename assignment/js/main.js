87e8a5654cfaf54f434c029af5cc6d996747a5b4
Alex ZJ Zhou <47014551+zjalexzhou@users.noreply.github.com>
2021-04-16 23:18:33 +0800

    Finished

--- assignment/js/main.js
+++ assignment/js/main.js
@@ -3,20 +3,24 @@
 ===================== */
 
 var zipInfo = [];
-var ctx = document.getElementById('myChart').getContext('2d');
-var myChart;
+var ctx1 = document.getElementById('chart-bar').getContext('2d');
+var ctx2 = document.getElementById('chart-doughnut').getContext('2d');
+var bar_chart;
+var doughnut_chart;
+var stats = [];
+var found = {};
 
 vaccinationPath = 'https://raw.githubusercontent.com/zjalexzhou/OSGIS-week9/master/assignment/data/vaccination_by_zip.json'
 zipcodePath = 'https://raw.githubusercontent.com/CPLN692-MUSA611-Open-Source-GIS/datasets/master/geojson/Zipcodes_Poly.geojson'
 populationPath = 'https://raw.githubusercontent.com/zjalexzhou/OSGIS-week9/master/assignment/data/population_by_zip.json'
 
 var doughnutChart = function(data){
-  myChart = new Chart(ctx, {
+  var myChart = new Chart(ctx2, {
       type: 'doughnut',
       data: {
           labels: ['Fully Vaccinated', 'Partially Vaccinated', 'Unvaccinated'],
           datasets: [{
-              label: 'Vaccination Information for Zipcode' + toString(data.zipcode),
+              label: 'Vaccination Information for zipcode ' + data.zipcode,
               data: [data.fully_vaccinated, data.partially_vaccinated, data.unvaccianted],
               backgroundColor: [
                   'rgba(54, 162, 235, 0.2)',
@@ -40,10 +44,43 @@ var doughnutChart = function(data){
   });
 }
 
+var barChart = function(data){
+  var myChart = new Chart(ctx1, {
+      type: 'bar',
+      data: {
+          labels: ['Fully Vaccinated', 'Partially Vaccinated', 'Unvaccinated'],
+          datasets: [{
+              label: 'Vaccination Information for zipcode ' + data.zipcode,
+              data: [data.fully_vaccinated, data.partially_vaccinated, data.unvaccianted],
+              backgroundColor: [
+                  'rgba(54, 162, 235, 0.2)',
+                  'rgba(255, 206, 86, 0.2)',
+                  'rgba(255, 99, 132, 0.2)',
+                  // 'rgba(75, 192, 192, 0.2)',
+                  // 'rgba(153, 102, 255, 0.2)',
+                  // 'rgba(255, 159, 64, 0.2)'
+              ],
+              borderColor: [
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(255, 99, 132, 1)',
+                  // 'rgba(75, 192, 192, 1)',
+                  // 'rgba(153, 102, 255, 1)',
+                  // 'rgba(255, 159, 64, 1)'
+              ],
+          }]
+      },
+  });
+}
+
 var removeChart = function() {
-  if(_.isUndefined(myChart)){}
+  if(_.isUndefined(doughnut_chart)){}
   else{
-    myChart.destroy();
+    doughnut_chart.destroy();
+  }
+  if(_.isUndefined(bar_chart)){}
+  else{
+    bar_chart.destroy();
   }
 }
 
@@ -58,15 +95,66 @@ var eachFeatureFunction = function(layer) {
         })
         if(chartData[0].vaccinated_ratio >= 0){
           doughnutChart(chartData[0])
+          barChart(chartData[0])
+          $('#text-for-NA-data').text('Hover your mouse over the chart elements for details.')
+        } else {
+          $('#text-for-NA-data').text('Sorry, we currently do not have vaccination information for regions represented by this zipcode.')
         }
+        $('h2').text(`For zipcode ${zip}`);
+        $('#data-brief').show();
+        $('#population').text(chartData[0].total_population)
+        if(_.isNumber(chartData[0].vaccinated_ratio)){
+          $('#vaccinated-ratio').text(math.round(chartData[0].vaccinated_ratio,3)*100 + '%')
+        } else {
+          $('#vaccinated-ratio').text(chartData[0].vaccinated_ratio)
+        }
+        $('#fully-vaccinated').text(chartData[0].fully_vaccinated)
+        $('#partially-vaccinated').text(chartData[0].partially_vaccinated)
+        $('#unvaccinated').text(chartData[0].unvaccianted)
     });
   };
+  
+var getDesStats = function(property_name){
+  let stats_dataset = []
+  _.each(zipInfo, function(e){
+    if(e[property_name] >= 0){
+      stats_dataset.push(e[property_name])
+    } else {
+      stats_dataset.push(0)
+    }
+  })
+  min_elem = math.min(stats_dataset)
+  median_elem = math.median(stats_dataset)
+  // median_elem = stats_dataset[stats_dataset.length/2]
+  max_elem = math.max(stats_dataset)
+  return [min_elem, median_elem, max_elem]
+}
+
+var getColorRamp = function(dat, min, median, max){
+  startColor = '#000000'
+  medColor = '#ffff00'
+  endColor = '#0000ff'
+  colorRamp = chroma.scale([startColor, medColor, endColor]).domain([min, median, max]);
+  return colorRamp(dat).hex();
+} 
 
 var vaccinated_ratio_style = function(feature){
+  stats = getDesStats('vaccinated_ratio');
+  found = zipInfo.find(x => x.zipcode == feature.properties.CODE);
+  // console.log(found)
+  if (found != undefined) {
+    val = found.vaccinated_ratio;
+  } else {
+    val = 0
+  }
+  // console.log(val)
+  // console.log(String(getColorRamp(zipInfo[feature.properties.CODE], stats[0], stats[1], stats[2])))
+  return {fillOpacity: 0.9,
+    color: String(getColorRamp(val, stats[0], stats[1], stats[2])),
+    fillColor: String(getColorRamp(val, stats[0], stats[1], stats[2]))}
 }
 
 $(document).ready(function() {
-
     // Request zipcode data (with polygon features)
     $.ajax(zipcodePath).done(function(data) {
         let zipcode = JSON.parse(data);
@@ -79,12 +167,6 @@ $(document).ready(function() {
           zipList.push(e.properties.CODE)
         })
         // console.log(ziplist)
-        // Plot zipcode polygons
-        let polygon  = L.geoJson(zipcode, {
-          style: vaccinated_ratio_style
-          }).addTo(map);
-        console.log(polygon)
-        polygon.eachLayer(eachFeatureFunction)
         // Request vaccination data
         $.ajax(vaccinationPath).done(function(data) {
           let vaccination = JSON.parse(data);
@@ -114,6 +196,12 @@ $(document).ready(function() {
                 }
               }
               zipInfo.push(eachZipInfo)
+              // Plot zipcode polygons
+              let polygon  = L.geoJson(zipcode, {
+                style: vaccinated_ratio_style
+                }).addTo(map);
+              // console.log(polygon)
+              polygon.eachLayer(eachFeatureFunction)              
             })
           })
         })
